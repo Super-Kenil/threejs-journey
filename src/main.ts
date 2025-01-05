0d7c05c891d56adcfab3ab933dd2021520a5f389
Super-Kenil <kenilsudani.blog@gmail.com>
2025-01-05 22:53:47 +0530

    Triangle rotation Triangle rotation reorder Triangle scale Camera Lookat

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 
 const canvasEl = document.getElementById('webgl-canvas')!
-console.info(canvasEl)
+// console.info(canvasEl)
 console.info('Threejs:', THREE)
 
 // Scene
@@ -12,15 +12,24 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 
+// position objects
+mesh.position.set(0.7, -0.6, 1)
 
-mesh.position.set(0.7,-0.6,1)
+// Scale objects
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotating Objects
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI * 0.5
+mesh.rotation.x = Math.PI * 1.2
+mesh.rotation.z = 0.2
 
 /*console.info('mesh position', mesh.position.length())*/
 mesh.position.normalize()
 scene.add(mesh)
 
 /* Axes Helper*/
-const axesHelper = new THREE.AxesHelper(4)
+const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 // Sizes
@@ -30,9 +39,10 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(1, 2, -1)
+camera.position.set(0, 0, 3)
+camera.lookAt(mesh.position)
 
-camera.rotateOnAxis(camera.position,0.45)
+// camera.rotateOnAxis(camera.position,0.05)
 /*console.info('distance to', mesh.position.distanceTo(camera.position))*/
 scene.add(camera)
 
